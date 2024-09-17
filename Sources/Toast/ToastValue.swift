256b02881ea8f43fec9455778af54de138ba6a97
Sunghyun Kim <sunghyun_k@icloud.com>
2024-09-17 21:17:22 +0900

    icon AnyView로 변경

--- Sources/Toast/ToastValue.swift
+++ Sources/Toast/ToastValue.swift
@@ -1,32 +1,32 @@
 import Foundation
+import SwiftUI
 
 import struct SwiftUI.Image
 import struct SwiftUI.Color
-
 public struct ToastValue {
-  internal var icon: Image?
+  internal var icon: AnyView?
   internal var message: String
   internal var button: ToastButton?
   internal var duration: TimeInterval?
   public init(
-    icon: Image? = nil,
+    icon: (any View)? = nil,
     message: String,
     button: ToastButton? = nil,
     duration: TimeInterval = 3.0
   ) {
-    self.icon = icon
+    self.icon = icon.map { AnyView($0) }
     self.message = message
     self.button = button
     self.duration = min(max(0, duration), 10)
   }
   @_disfavoredOverload
   internal init(
-    icon: Image? = nil,
+    icon: (any View)? = nil,
     message: String,
     button: ToastButton? = nil,
     duration: TimeInterval? = nil
   ) {
-    self.icon = icon
+    self.icon = icon.map { AnyView($0) }
     self.message = message
     self.button = button
     self.duration = duration
